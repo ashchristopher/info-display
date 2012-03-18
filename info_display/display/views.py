0dbafe8c8a98dd26330382be7d23deae3c29c773
Ash Christopher <ash.christopher@gmail.com>
2012-03-18 15:19:12 -0700

    If user isn't logged in, they can't see the data

--- info_display/display/views.py
+++ info_display/display/views.py
@@ -1,4 +1,7 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
 
 from display.models import SubscriberCount, YesterdaysSignupsCount
 from display.mixins import JSONResponseMixin
@@ -10,7 +13,9 @@ class HomeView(TemplateView):
 
     template_name = 'display/home.html'
 
-    def get(self, request):
+
+    def get_context_data(self):
+        """ Get the context with subscriber info populated. """
         try:
             subscribers = SubscriberCount.objects.latest('date_added')
         except SubscriberCount.DoesNotExist:
@@ -20,12 +25,25 @@ class HomeView(TemplateView):
             yesterdays_signups = YesterdaysSignupsCount.objects.latest('date_added')
         except YesterdaysSignupsCount.DoesNotExist:
             yesterdays_signups = None
- 
+
         context = {
             'total_subscribers' : getattr(subscribers, 'total_subscribers', NO_DATA),
             'yesterdays_signups' : getattr(yesterdays_signups, 'subscribers', NO_DATA),
         }
- 
+
+        return context
+
+    def get(self, request):
+
+        # only let staff accounts view the data.
+        if not request.user.is_staff:
+            context = {
+                'total_subscribers' : '--',
+                'yesterdays_signups' : '--',
+            }
+        else:
+            context = self.get_context_data()
+
         return self.render_to_response(context)
 
 
